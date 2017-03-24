69a1958428af4692a587e642445ca20a34e53e61
BenPhillip <p627121110@163.com>
2017-03-25 00:15:25 +0800

    Saved when the device is rotated graphics

--- app/src/main/java/com/example/gzp/draganddraw/Box.java
+++ app/src/main/java/com/example/gzp/draganddraw/Box.java
@@ -1,12 +1,17 @@
 package com.example.gzp.draganddraw;
 
 import android.graphics.PointF;
+import android.os.Parcel;
+import android.os.Parcelable;
 
 /**
  * Created by Ben on 2017/3/23.
+ * 为Box实现Parcelable接口
+ *然后我们就可以在View.onSaveInstanceState()方法中存储 ArrayList of Objects: mBoxes，调用
+ *Bundle.putParcelableArrayList(String key, ArrayList<? extends Parcelable> value)
  */
 
-public class Box {
+public class Box implements Parcelable{
     private PointF mCurrent;
     private PointF mOrigin;
 
@@ -15,6 +20,25 @@ public class Box {
         mCurrent=origin;
         mOrigin=origin;
     }
+
+    protected Box(Parcel in) {
+        //读取的顺序一定要和写出的顺序一致
+        mCurrent =(PointF) in.readValue(null);
+        mOrigin=(PointF) in.readValue(null);
+
+    }
+
+    public static final Parcelable.Creator<Box> CREATOR = new Parcelable.Creator<Box>() {
+        @Override
+        public Box createFromParcel(Parcel in) {
+            return new Box(in);
+        }
+
+        @Override
+        public Box[] newArray(int size) {
+            return new Box[size];
+        }
+    };
 
     public PointF getOrigin() {
         return mOrigin;
@@ -27,4 +51,14 @@ public class Box {
     }
 
 
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    @Override
+    public void writeToParcel(Parcel dest, int flags) {
+        dest.writeValue(mCurrent);
+        dest.writeValue(mOrigin);
+    }
 }
--- app/src/main/java/com/example/gzp/draganddraw/BoxDrawingView.java
+++ app/src/main/java/com/example/gzp/draganddraw/BoxDrawingView.java
@@ -4,13 +4,15 @@ import android.content.Context;
 import android.graphics.Canvas;
 import android.graphics.Paint;
 import android.graphics.PointF;
+import android.os.Bundle;
+import android.os.Parcelable;
 import android.util.AttributeSet;
 import android.util.Log;
 import android.view.MotionEvent;
 import android.view.View;
 
 import java.util.ArrayList;
-import java.util.List;
+
 
 /**
  * Created by Ben on 2017/3/23.
@@ -18,9 +20,11 @@ import java.util.List;
 
 public class BoxDrawingView extends View {
     private static final String TAG = "BoxDrawingView";
+    private static final String SAVED_SUPER_VIEW ="savedInstanceState" ;
+    private static final String SAVED_BOX="boxen";
 
     private Box mCurrentBox;
-    private List<Box> mBoxen = new ArrayList<>();
+    private ArrayList<Box> mBoxen = new ArrayList<>();
     private Paint mBoxPaint;
     private Paint mBackgroundPaint;
 
@@ -85,9 +89,39 @@ public class BoxDrawingView extends View {
             float bottom = Math.max(box.getOrigin().y, box.getCurrent().y);
 
             canvas.drawRect(left, top, right, bottom, mBoxPaint);
-
         }
     }
 
+    /**
+     * View视图有ID时，才可以调用它们
+     *需要保存BoxDrawingView的View父视图的状态。
+     * 在Bundle中保存super.onSaveInstanceState()方法结果，
+     * 然后调用super.onRestoreInstanceState(Parcelable)方法把结果发送给超类。
+     * @return 将数据保存在Bundle中
+     */
+    @Override
+    protected Parcelable onSaveInstanceState() {
+        Log.i(TAG, "onSaveInstanceState: saved sucesess");
+        super.onSaveInstanceState();
+
+        Bundle bundle= new Bundle();
+        bundle.putParcelable(SAVED_SUPER_VIEW,super.onSaveInstanceState());
+        bundle.putParcelableArrayList(SAVED_BOX,mBoxen);
+
+        return bundle;
+
+    }
+
+    @Override
+    protected void onRestoreInstanceState(Parcelable state) {
+        Log.i(TAG, "onRestoreInstanceState: saved");
+        if(state instanceof Bundle){
+            Bundle bundle=(Bundle)state;
+            state = bundle.getParcelable(SAVED_SUPER_VIEW);
+            mBoxen = bundle.getParcelableArrayList(SAVED_BOX);
+            Log.i(TAG, "onRestoreInstanceState: "+mBoxen.size());
+        }
+        super.onRestoreInstanceState(state);
+    }
 
 }
